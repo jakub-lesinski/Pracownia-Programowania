b4856b2d053422928490b41fd8ce190ddc0eac0b
jakub-lesinski <jak-les@wp.pl>
2020-02-28 12:05:50 +0100

    Wprowadzenie projektu

---
+++ Prog/Polygon.cpp
@@ -0,0 +1,20 @@
+/*! \file Punkt2.h
+	*
+	* \brief Zawiera definicjê klasy Polygon
+	*
+	* Plik zawiera implementacjê metod klasy Polygo.
+	*
+	* \author Jakub Lesiñski
+	* \date 2020.02.17
+	* \version 1.00.00
+	*/
+
+#include "Polygon.h"
+
+void Polygon::setVertices(Punkt2* vertices, int count) {
+
+}
+
+double getTriangleArea(Punkt2 p1, Punkt2 p2, Punkt2 p3) {
+	return
+}
---
+++ Prog/Źródło.cpp
@@ -0,0 +1,25 @@
+/*! \file main.cpp
+	*
+	* \brief Kod programu testuj¹cego klasê Punkt2, Polygon
+	*
+	* Plik zawiera funkcjê main(),
+	* w której wykonano kilka podstawowych testów
+	* dotycz¹cych klasy Punkt2 oraz Polygon
+	*
+	* \author Jakub Lesiñski
+	* \date 2020.02.24
+	* \version 1.00.00
+	*/
+
+#include "Punkt2.h"
+#include "Polygon.h"
+
+int main() {
+	Punkt2* vertices = new Punkt2;
+
+	setVertices(punkty, n);
+
+
+
+	return 0;
+}
